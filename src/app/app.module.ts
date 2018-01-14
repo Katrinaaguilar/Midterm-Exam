c4fc789557ccb56785d086513375137034e39084
KatrinaDawnAguilar <kat15aguilar@yahoo.com>
2018-01-14 17:24:44 +0800

    modified app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {environment} from '../environments/environment';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
 
