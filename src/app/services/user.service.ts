77332dc815db0cb7cda71f875cd3e1475480a018
KatrinaDawnAguilar <kat15aguilar@yahoo.com>
2018-01-14 18:14:14 +0800

    create getUsers

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@angular/core';
 import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
 import {User} from '../models/User';
 import {Observable} from 'rxjs/Observable';
+
 @Injectable()
 export class UserService {
   usersCollection: AngularFirestoreCollection<User>;
   users:Observable<User[]>;
+  
   constructor(public afs: AngularFirestore) {
     this.users = this.afs.collection('users').valueChanges();
+   }
+
+   getUsers(){
+     return this.users;
    }
     
 }
