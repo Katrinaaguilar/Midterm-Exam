6f7c97c53df6351b5fd2713ed5dab59c89ce6343
KatrinaDawnAguilar <kat15aguilar@yahoo.com>
2018-01-14 18:03:36 +0800

    create user.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
 
 @Injectable()
 export class UserService {
+  usersCollection: AngularFirestoreCollection<User>;
 
-  constructor() { }
+  constructor(public afs: AngularFirestore) { }
 
+}
+interface User{
+  id?: string;
+  name?: string;
+  email?: string;
+  password?:string;
 }
